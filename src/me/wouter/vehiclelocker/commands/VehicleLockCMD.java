6de284ffae77ad1d0ed964adc0238614c7201345
TheMelvinNL <melvinsnijders@kpnmail.nl>
2019-01-19 21:14:41 +0100

    Changed set method

--- src/me/wouter/vehiclelocker/commands/VehicleLockCMD.java
+++ src/me/wouter/vehiclelocker/commands/VehicleLockCMD.java
@@ -10,20 +10,21 @@ import me.wouter.vehiclelocker.data.DataFile;
 
 public class VehicleLockCMD implements CommandExecutor {
 
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage("This command is for players only.");
-            return true;
-        }
-        Player p = (Player) sender;
-        boolean isLocked = DataFile.getInstance().getData().getBoolean(p.getUniqueId() + ".Locked");
-        if (!isLocked) {
-            DataFile.getInstance().getData().set(p.getUniqueId() + ".Locked", true);
-            p.sendMessage(Main.cc("Command.Locked"));
-        } else {
-            DataFile.getInstance().getData().set(p.getUniqueId() + ".Locked", false);
-            p.sendMessage(Main.cc("Command.Unlocked"));
-        }
-        return true;
-    }
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (!(sender instanceof Player)) {
+			sender.sendMessage("This command is for players only.");
+			return true;
+		}
+		Player p = (Player) sender;
+		boolean isLocked = DataFile.getInstance().getData().getBoolean(p.getUniqueId() + ".Locked");
+		if (!isLocked) {
+			DataFile.getInstance().getData().set(p.getUniqueId() + ".Locked", true);
+			p.sendMessage(Main.cc("Command.Locked"));
+		}else {
+			// Why do this, you ask? Because otherwise you'd have a useless line.
+			DataFile.getInstance().getData().set(p.getUniqueId() + ".Locked", null);
+			p.sendMessage(Main.cc("Command.Unlocked"));
+		}
+		return true;
+	}
 }
